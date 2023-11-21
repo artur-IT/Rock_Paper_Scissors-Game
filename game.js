1f9011d90ac59874c2010a26ddcbbcc65fe3cd30
Artur Matysiak <inz.matys@gmail.com>
2023-11-21 12:56:59 +0100

    Refactorize code, optimize css, change language to english

--- game.js
+++ game.js
@@ -1,29 +1,20 @@
 const img = document.querySelectorAll("img");
-let myPNK = ""; // MY CHOICE
-let aiPNK = ""; // AI CHOICE
+let myPNK = (aiPNK = ""); // MY CHOICE and AI CHOICE
 const tab = [...img]; // ARRAY WITH 3 ARG. P,N,K
-const papier = tab[0].dataset.option; // paper
-const kamien = tab[1].dataset.option; // stone
-const nozyczki = tab[2].dataset.option; // scissors
+const paper = tab[0].dataset.option; // paper
+const stone = tab[1].dataset.option; // stone
+const scissors = tab[2].dataset.option; // scissors
 // GAME RESULTS
-const iWin = "Wygrałem!";
-const aiWin = "Przegrałem :(";
-const draw = "Remis";
 const win = document.querySelector("h2 span[data-summary='who-win']");
 
-// COUNTERS:  PANEL-RIGHT
-let countWin = 0;
-let countLoss = 0;
-let countDraw = 0;
-let countGame = 0;
+// COUNTERS (PANEL-RIGHT)
+countWin = countLoss = countDraw = countGame = 0;
 
 // MY CHOICE
 function Choice() {
   img.forEach((item) =>
     item.addEventListener("click", () => {
-      img.forEach((i) => {
-        i.style.boxShadow = ""; // CLEAR BORDER
-      });
+      img.forEach((i) => (i.style.boxShadow = "")); // CLEAR BORDER
       myPNK = item.dataset.option;
       item.style.boxShadow = "0 0 0 0" + 5 + "px chartreuse";
       // AI CHOICE
@@ -39,23 +30,15 @@ function Choice() {
 // CHECK CONDITIONS WIN
 function Check() {
   if (myPNK == aiPNK) {
-    win.textContent = draw; // DRAW
+    win.textContent = "Draw"; // DRAW
     win.style.color = "orange";
     ++countDraw;
-  } else if (
-    (myPNK == papier && aiPNK == kamien) ||
-    (myPNK == kamien && aiPNK == nozyczki) ||
-    (myPNK == nozyczki && aiPNK == papier)
-  ) {
-    win.textContent = iWin; // USER WIN
-    win.style.color = "lime";
+  } else if ((myPNK == paper && aiPNK == stone) || (myPNK == stone && aiPNK == scissors) || (myPNK == scissors && aiPNK == paper)) {
+    win.textContent = "I Won!"; // USER WON
+    win.style.color = "green";
     ++countWin;
-  } else if (
-    (myPNK == papier && aiPNK == nozyczki) ||
-    (myPNK == kamien && aiPNK == papier) ||
-    (myPNK == nozyczki && aiPNK == kamien)
-  ) {
-    win.textContent = aiWin; // AI WIN
+  } else if ((myPNK == paper && aiPNK == scissors) || (myPNK == stone && aiPNK == paper) || (myPNK == scissors && aiPNK == stone)) {
+    win.textContent = "I Lost :("; // AI WON
     win.style.color = "#a00";
     ++countLoss;
   }
@@ -63,22 +46,18 @@ function Check() {
 
 // FUNCTION PLAY
 function Start() {
-  const btn = document.querySelector("button");
   // PANEL LEFT
   const myChoice = document.querySelector("p span[data-summary='your-choice']");
   const aiChoice = document.querySelector("p span[data-summary='ai-choice']");
-
-  btn.addEventListener("click", () => {
+  document.querySelector("button").addEventListener("click", () => {
     if (myPNK == "") {
-      alert("Wybierz Dłoń!");
+      alert("Choose Hand");
       return; // STOP FUNCTION
     }
-    img.forEach((i) => {
-      i.style.boxShadow = "";
-    });
+    img.forEach((i) => (i.style.boxShadow = ""));
     myChoice.textContent = myPNK; // SHOW MY CHOICE
     aiChoice.textContent = aiPNK; // SHOW AI CHOICE
-    Check(); // CHECK WHO WIN
+    Check(); // CHECK WHO WON
     document.querySelector("p.numbers span").textContent = ++countGame;
     document.querySelector("p.wins span").textContent = countWin;
     document.querySelector("p.losses span").textContent = countLoss;
@@ -98,4 +77,3 @@ Choice();
      b. kto wygrał win/loss/draw (zbudować warunki porównujące)
 3. Zaktualizować dane o grze (lewa + prawa kolumna)
 */
-// 100% my work :)
